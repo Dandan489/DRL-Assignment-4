09c5d75063505ebc392a16b9329377ba9ef87907
DanDan <dan0922.dan0922@gmail.com>
2025-05-06 21:24:14 +0800

    Upload first version

--- Q3/student_agent.py
+++ Q3/student_agent.py
@@ -1,11 +1,201 @@
 import gymnasium as gym
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal
+from collections import deque
+import random
+
+device = torch.device("cpu")
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+    
+    def push(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+    
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
+        return states, np.array(actions), np.array(rewards), next_states, np.array(dones)
+    
+    def __len__(self):
+        return len(self.buffer)
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=256, log_std_min=-20, log_std_max=2):
+        super(Actor, self).__init__()
+        
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+        
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        
+        self.mu = nn.Linear(hidden_dim, action_dim)
+        self.log_std = nn.Linear(hidden_dim, action_dim)
+        
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        
+        mu = self.mu(x)
+        
+        log_std = self.log_std(x)
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+        
+        return mu, log_std
+    
+    def sample(self, state):
+        mu, log_std = self.forward(state)
+        std = log_std.exp()
+        
+        normal = Normal(mu, std)
+        z = normal.rsample()
+        action = torch.tanh(z)
+        
+        log_prob = normal.log_prob(z) - torch.log(1 - action.pow(2) + 1e-6)
+        log_prob = log_prob.sum(1, keepdim=True)
+        
+        return action, log_prob
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=256):
+        super(Critic, self).__init__()
+        
+        self.fc1 = nn.Linear(state_dim + action_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.q1 = nn.Linear(hidden_dim, 1)
+        
+        self.fc3 = nn.Linear(state_dim + action_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, hidden_dim)
+        self.q2 = nn.Linear(hidden_dim, 1)
+        
+    def forward(self, state, action):
+        x = torch.cat([state, action], 1)
+        
+        q1 = F.relu(self.fc1(x))
+        q1 = F.relu(self.fc2(q1))
+        q1 = self.q1(q1)
+        
+        q2 = F.relu(self.fc3(x))
+        q2 = F.relu(self.fc4(q2))
+        q2 = self.q2(q2)
+        
+        return q1, q2
+
+class SACAgent:
+    def __init__(
+        self, 
+        state_dim,
+        action_dim,
+        max_action,
+        buffer_capacity=1000000,
+        batch_size=256,
+        discount=0.99,
+        tau=0.005,
+        initial_random_steps=10000,
+        policy_update_frequency=2,
+        alpha=0.2,
+        auto_entropy_tuning=True,
+        hidden_dim=256,
+        lr_actor=3e-4,
+        lr_critic=3e-4,
+        lr_alpha=3e-4
+    ):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.max_action = max_action
+        
+        self.discount = discount
+        self.tau = tau
+        self.batch_size = batch_size
+        self.initial_random_steps = initial_random_steps
+        self.policy_update_frequency = policy_update_frequency
+        
+        self.actor = Actor(self.state_dim, self.action_dim, hidden_dim).to(device)
+        self.critic = Critic(self.state_dim, self.action_dim, hidden_dim).to(device)
+        self.critic_target = Critic(self.state_dim, self.action_dim, hidden_dim).to(device)
+        
+        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
+            target_param.data.copy_(param)
+            
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=lr_actor)
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=lr_critic)
+        
+        self.auto_entropy_tuning = auto_entropy_tuning
+        if self.auto_entropy_tuning:
+            self.target_entropy = -self.action_dim  # -dim(A)
+            self.log_alpha = torch.zeros(1, requires_grad=True, device=device)
+            self.alpha_optimizer = optim.Adam([self.log_alpha], lr=lr_alpha)
+            self.alpha = self.log_alpha.exp().item()
+        else:
+            self.alpha = alpha
+            
+        self.replay_buffer = ReplayBuffer(buffer_capacity)
+        self.train_start = 100000
+        
+        self.total_steps = 0
+        self.episode_steps = 0
+        self.episodes_returns = []
+        self.actor_losses = []
+        self.critic_losses = []
+        self.alpha_losses = []
+        
+    def select_action(self, state, evaluate=False):
+        state = torch.FloatTensor(state).to(device).unsqueeze(0)
+        
+        with torch.no_grad():
+            mu, _ = self.actor(state)
+            action = torch.tanh(mu)
+            
+        return action.detach().cpu().numpy()[0]
+
+    def save_model(self, path):
+        torch.save({
+            'actor': self.actor.state_dict(),
+            'critic': self.critic.state_dict(),
+            'critic_target': self.critic_target.state_dict(),
+            'actor_optimizer': self.actor_optimizer.state_dict(),
+            'critic_optimizer': self.critic_optimizer.state_dict(),
+            'alpha_optimizer': self.alpha_optimizer.state_dict(),
+            'log_alpha': self.log_alpha,
+            'total_steps': self.total_steps,
+        }, path)
+    
+    def load_model(self, path):
+        checkpoint = torch.load(path)
+        self.actor.load_state_dict(checkpoint['actor'])
+        self.critic.load_state_dict(checkpoint['critic'])
+        self.critic_target.load_state_dict(checkpoint['critic_target'])
+        self.actor_optimizer.load_state_dict(checkpoint['actor_optimizer'])
+        self.critic_optimizer.load_state_dict(checkpoint['critic_optimizer'])
+        self.alpha_optimizer.load_state_dict(checkpoint['alpha_optimizer'])
+        self.log_alpha = checkpoint['log_alpha'].to(device).detach().requires_grad_()
+        self.alpha = self.log_alpha.exp().item()
+        self.total_steps = checkpoint['total_steps']
 
 # Do not modify the input of the 'act' function and the '__init__' function. 
 class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
         self.action_space = gym.spaces.Box(-1.0, 1.0, (21,), np.float64)
+        self.agent = SACAgent(
+            67, 21, 1.0,
+            buffer_capacity=1000000,
+            batch_size=256,
+            discount=0.99,
+            tau=0.005,
+            initial_random_steps=0,
+            policy_update_frequency=2,
+            auto_entropy_tuning=True,
+            hidden_dim=256
+        )
+        self.agent.load_model("sac_final_model")
 
     def act(self, observation):
-        return self.action_space.sample()
+        action = self.agent.select_action(observation)
+        return action
